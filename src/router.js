71e768d1b3f3e4e44e49d8d2c31a4c691808b818
changlingling <changlingling@zybang.com>
2019-10-17 13:50:32 +0800

    搜索部分开始

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './components/Home.vue'
+import Search from './components/search.vue'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/search',
+      name: 'search',
+      component: Search
     }
   ]
 })
